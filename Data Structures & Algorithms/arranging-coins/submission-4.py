0f4cbe404a4b739431a475b6ff11f88d5c3d97f7
thomasrotchford <157422184+thomasrotchford@users.noreply.github.com>
2026-06-08 15:22:57 -0700

    Add: arranging-coins - submission-4

---
+++ Data Structures & Algorithms/arranging-coins/submission-4.py
@@ -0,0 +1,40 @@
+class Solution:
+    def arrangeCoins(self, n: int) -> int:
+
+        def calcN(q):
+            return q*(q+1)*0.5     
+
+
+
+        left=0
+
+
+
+        right=n
+
+        while left<=right :
+
+            mid=(left+right)//2
+
+            N=calcN(mid)
+
+            if N==n:
+                return mid
+
+            elif N>n:
+                if left==mid:
+                    return mid-1
+                else:
+                    right=mid-1
+
+            elif N<n:
+
+                left=mid+1
+                
+        return mid
+
+
+
+
+            
+        
