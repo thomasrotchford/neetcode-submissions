a3125d78abbd9e7b76d3b5254b7eb856fdefe568
thomasrotchford <157422184+thomasrotchford@users.noreply.github.com>
2026-06-16 14:21:38 -0700

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,46 @@
+class Solution:
+    ##numdelimword
+    def encode(self, strs: List[str]) -> str:
+
+        encoded_string=[]
+
+        for word in strs:
+            encoded_string.append(str(len(word)))
+            encoded_string.append(";")
+            encoded_string.append(word)
+
+
+        return "".join(encoded_string)
+
+    def decode(self, s: str) -> List[str]:
+
+        decoded_string=[]
+
+        wordInd=0
+
+        leng=""
+
+        ADD=False
+
+        for ind in range(len(s)):
+
+
+            if not ADD and ord(s[ind])>=ord('0') and ord(s[ind])<=ord('9'):
+                leng=leng+s[ind]
+            elif s[ind]==";" or ADD:
+                ADD=True
+                
+                if s[ind]==";":
+                    counter=int(leng)
+                    decoded_string.append("")
+                    decoded_string[wordInd]=s[ind+1:ind+int(leng)+1]
+                    wordInd=wordInd+1
+                    leng=""
+                else:
+                  counter=counter-1
+                if counter==0:
+                    ADD=False
+                
+
+        
+        return decoded_string
